18191d128ff3adbb271a3468a03b1d8be05abf30
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-09-04 10:55:19 +0200

    [UPD] aggiunti i metodi di ricerca e filtraggio

--- src/components/ProductsCards.jsx
+++ src/components/ProductsCards.jsx
@@ -6,6 +6,12 @@ const ProductsCards = () => {
     // variabile di stato per la ricerca per titolo
     const [searchProduct, setSearchProduct] = useState('')
 
+    // variabile di stato per la ricerca di genere
+    const [searchGenre, setSearchGenre] = useState('')
+
+    // variabile di stato per ordinamento A-Z Z-A
+    const [order, setOrder] = useState('A-Z')
+
     // variabile di stato per i dati dell'API
     const [products, setProducts] = useState([])
 
@@ -30,8 +36,23 @@ const ProductsCards = () => {
         getProducts()
     }, [])
 
+    // ricerca per genere
+    const filteredGenre = searchGenre ? products.filter(p => p.category === searchGenre) : products;
+
     // ricerca per titolo
-    const filteredProducts = products.filter(p => p.title.toLowerCase().includes(searchProduct.toLocaleLowerCase()))
+    const filteredProducts = filteredGenre.filter(p => p.title.toLowerCase().includes(searchProduct.toLocaleLowerCase()))
+
+    // ordinamento alfabetico
+    const orderedProducts = [...filteredProducts].sort((a, b) => {
+        if (order === 'A-Z') {
+            return a.title.localeCompare(b.title);
+        } else if (order === 'Z-A') {
+            return b.title.localeCompare(a.title);
+        } else {
+            return 0;
+        }
+    });
+
 
     return (
         <>
@@ -46,30 +67,33 @@ const ProductsCards = () => {
                 />
 
                 <p>Ordina da:</p>
-                <select>
+                <select value={order} onChange={(e) => setOrder(e.target.value)}>
                     <option value="A-Z">A-Z</option>
                     <option value="Z-A">Z-A</option>
                 </select>
 
                 <p>Ordina per Categoria</p>
-                <select>
+                <select value={searchGenre} onChange={(e) => setSearchGenre(e.target.value)} >
+                    <option value="">Tutti</option>
                     <option value="smartphone">Smartphone</option>
                     <option value="tablet">Tablet</option>
                     <option value="smartwatch">Smartwatch</option>
-                    <option value="electricscooter">Electricscooter</option>
+                    <option value="electric-scooter">Electricscooter</option>
                 </select>
             </div>
-            {filteredProducts.map((p, index) => {
-                return (
 
-                    <div className='col-lg-3 col-md-4 col-sm-6 mb-4' key={index}>
-                        <div className='card p-2 h-100'>
-                            <h5>{p.title}</h5>
-                            <p>{p.category} {p.category === 'tablet' ? '🖥️' : p.category === 'smartphone' ? '📱' : p.category === 'smartwatch' ? '⌚' : '🛴'}</p>
+            {orderedProducts.length === 0 ? <div className='text-white fs-2 mt-3'>Nessun risultato trovato..</div>
+                : orderedProducts.map((p, index) => {
+                    return (
+
+                        <div className='col-lg-3 col-md-4 col-sm-6 mb-4' key={index}>
+                            <div className='card p-2 h-100'>
+                                <h5>{p.title}</h5>
+                                <p>{p.category} {p.category === 'tablet' ? '🖥️' : p.category === 'smartphone' ? '📱' : p.category === 'smartwatch' ? '⌚' : '🛴'}</p>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
         </>
     )
 }
