b833815cbbb5a4734573befb6f575552d880789a
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-09-09 19:07:16 +0200

    [UPD] aggiunto badge di quantità per il comparatore e la pagina preferiti

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useFavorites } from '../context/FavoritesContext'
+import { useCompare } from '../context/CompareContext'
 
 const Header = () => {
+
+    // destrutturazione del custom hook per i preferiti
+    const { favoritiesIds } = useFavorites();
+
+    // destrutturo dal custom hook la variabile con gli id e la funzione di clear
+    const { compareIds } = useCompare();
+
     return (
         <>
             <header>
                 <Link to={'/'}><h2 className='text-white custom-b-l'>TechnoShop 🖥️</h2></Link>
-                <div className='d-flex align-items-center'>
-                    <Link className='no-decoration custom-b-r fs-5' to={'/confrontaPrezzi'}>Confronta <i class="fa-solid fa-scale-unbalanced-flip"></i></Link>
-                    <Link className='no-decoration' to={'/favorites'}><p className='fs-2 custom-b-r'>⭐</p></Link>
+
+                <div className='d-flex align-items-center custom-b-r'>
+
+                    <Link className='no-decoration custom-b-r fs-5 position-relative' to={'/confrontaPrezzi'}>
+                        Confronta<i className="fa-solid fa-scale-unbalanced-flip ms-2"></i>
+                        {compareIds.length > 0 && (
+                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                {compareIds.length}
+                            </span>
+                        )}</Link>
+
+                    <Link to="/favorites" className="position-relative text-white text-decoration-none ms-3">
+                        <i className="fa-solid text-warning fs-2"><p>⭐</p></i>
+                        {favoritiesIds.length > 0 && (
+                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                {favoritiesIds.length}
+                            </span>
+                        )}
+                    </Link>
+
                 </div>
 
             </header >
